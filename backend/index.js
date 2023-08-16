bf8d4cd647487cdb7605e30945535c899fe07500
Sameer <58684781+Sam007k@users.noreply.github.com>
2023-08-16 13:45:07 +0000

    backend_users_&_pins_setup_complete

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,26 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const pinRoute = require('./routes/pins')
+const userRoute = require('./routes/users')
+
 
 dotenv.config()
 
+
 const app = express()
+
+app.use(express.json())
+
 
 mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log('MONGODB_Connected')
 }).catch((err)=> console.log(err))
 
-app.listen(8000,()=>{
+app.use('/api/pins',pinRoute)
+app.use('/api/users',userRoute)
+
+
+app.listen(8800,()=>{
     console.log('Backend server is running!')
 })
