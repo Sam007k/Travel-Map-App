5f5824b98eb9770a610801d2ecdffeef32e3062c
Sam007k <msameer007k@gmail.com>
2023-11-13 18:12:43 +0530

    added vercel backend url to app.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,7 @@ function App() {
   useEffect(() => {
     const getPins = async () => {
       try {
-        const res = await axios.get('/pins')
+        const res = await axios.get('https://travel-map-app-chi.vercel.app/pins')
         setPins(res.data)
 
       } catch (err) {
