455757e0c1b5780a79336a2cb983c6abeafd0f25
Sam007k <msameer007k@gmail.com>
2023-11-13 17:57:01 +0530

    cors-fix

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,33 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const pinRoute = require('./routes/pins')
-const userRoute = require('./routes/users')
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const pinRoute = require("./routes/pins");
+const userRoute = require("./routes/users");
 
+dotenv.config();
+const cors = require("cors");
+const app = express();
 
-dotenv.config()
+app.use(
+  cors({
+    origin: ["https://travel-map-app-frontend-gpej4sywz-sam007k.vercel.app/"],
+    methods: ["POST", "PUT", "PATCH", "GET", "DELETE"],
+    credentials: true,
+  })
+);
 
+app.use(express.json());
 
-const app = express()
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MONGODB_Connected");
+  })
+  .catch((err) => console.log(err));
 
-app.use(express.json())
+app.use("/api/pins", pinRoute);
+app.use("/api/users", userRoute);
 
-
-mongoose.connect(process.env.MONGO_URL).then(()=>{
-    console.log('MONGODB_Connected')
-}).catch((err)=> console.log(err))
-
-app.use('/api/pins',pinRoute)
-app.use('/api/users',userRoute)
-
-
-app.listen(8800,()=>{
-    console.log('Backend server is running!')
-})
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
